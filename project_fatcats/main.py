0e233ce0d55db695591f5c5461968f940f9a07c9
huizhitan <S10244353@connect.np.edu.sg>
2022-08-02 17:57:42 +0800

    here!

---
+++ project_fatcats/main.py
@@ -0,0 +1,10 @@
+import api, coh, overheads, profit_loss
+
+def main():
+
+    forex = api.api_function()
+    overheads.overhead_function(forex)
+    coh.coh_function(forex)
+    profit_loss.profitloss_function(forex) 
+
+main()

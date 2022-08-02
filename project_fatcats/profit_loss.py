467f227692345606e68415dd5b5ed90f4abff553
Jasmin-pyt <S10240133@connect.np.edu.sg>
2022-08-02 21:02:14 +0800

    edits

--- project_fatcats/profit_loss.py
+++ project_fatcats/profit_loss.py
@@ -8,11 +8,10 @@ def profitloss_function(x):
        
     increase= True
     for p in range(1,len(data)-1):
-        if int(data[p+1][1])<int(data[p][1]):
+        if int(data[p+1][4])<int(data[p][4]):
             increase = False
-            deficit = int(data[p][1])- int(data[p+1][1])
+            deficit = int(data[p][4])- int(data[p+1][4])
             print('[PROFIT DEFICIT] DAY: '+data[p+1][0]+', AMOUNT: SGD'+str(deficit/x))
     if increase: 
         print('[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY')
-
 

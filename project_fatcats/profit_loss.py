3f7f1e0d0cae1f9e86143194bd44cf79c4275063
Jasmin-pyt <S10240133@connect.np.edu.sg>
2022-08-02 20:42:19 +0800

    Profit-loss final code

--- project_fatcats/profit_loss.py
+++ project_fatcats/profit_loss.py
@@ -2,11 +2,10 @@
 def profitloss_function(x):
     import csv
 
-    with open('csv_reports\profit-and-loss.csv', newline='') as f:
+    with open('csv_reports\profit-and-loss-usd.csv', newline='') as f:
         reader = csv.reader(f)
         data = list(reader)
        
-    x=1.5
     increase= True
     for p in range(1,len(data)-1):
         if int(data[p+1][1])<int(data[p][1]):
@@ -15,3 +14,5 @@ def profitloss_function(x):
             print('[PROFIT DEFICIT] DAY: '+data[p+1][0]+', AMOUNT: SGD'+str(deficit/x))
     if increase: 
         print('[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY')
+
+

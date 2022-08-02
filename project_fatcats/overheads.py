3893337f4e56ddd998ee6654a230594b52f9b141
Jasmin-pyt <S10240133@connect.np.edu.sg>
2022-08-02 18:23:47 +0800

    line?

--- project_fatcats/overheads.py
+++ project_fatcats/overheads.py
@@ -6,7 +6,7 @@ filepath = Path.cwd()/'csv_reports'/'overheads-day-90.csv'
 with filepath.open(mode = 'r', encoding = 'UTF-8', newline = '') as f:
     reader = f.read()
     for line in reader :
-        line = 
+        print(line)
        
 
         

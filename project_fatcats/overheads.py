d424aafd440689887767ba20d66458f71b37beb4
huizhitan <S10244353@connect.np.edu.sg>
2022-08-02 22:48:50 +0800

    grr

--- project_fatcats/overheads.py
+++ project_fatcats/overheads.py
@@ -7,10 +7,10 @@ filepath = Path.cwd()/'csv_reports'/'overheads-day-90.csv'
 read = filepath.open(mode = 'r', encoding = 'UTF-8', newline = '')
 next(read)
 reader = read.readlines()
-data = list(reader)
-for maxdata in range(1):
-    maxdata = float(data[2])
-    print(max(maxdata))
+data = dict(reader)
+for maxdata in range(0):
+    floating = float(maxdata)
+    print(floating)
     break
        
 

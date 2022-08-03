6afb063164f1a10c2df4b107a855f70a23ef7a5e
Jasmin-pyt <S10240133@connect.np.edu.sg>
2022-08-04 01:41:13 +0800

    first liner

--- project_fatcats/main.py
+++ project_fatcats/main.py
@@ -2,6 +2,7 @@ import api, coh, overhead, profit_loss
 
 def main():
     forex = api.api_function()
+    print(f"[REAL TIME CURRENCYCONVERSION RATE] USD1 = SGD {forex}")
     overhead.overhead_function(forex)
     coh.coh_function(forex)
     profit_loss.profitloss_function(forex)

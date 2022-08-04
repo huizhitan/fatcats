16e7ee513bbe60ce6539c5ab88323b54a0e982ce
Jasmin-pyt <S10240133@connect.np.edu.sg>
2022-08-04 11:12:25 +0800

    edit

--- project_fatcats/overhead.py
+++ project_fatcats/overhead.py
@@ -8,7 +8,7 @@ def overhead_function(x):
     
     highest = 1
     for p in range(1,len(data)-2):
-        if float(data[p+1][1])>float(data[p][1]):
+        if float(data[p+1][1])>float(data[highest][1]):
             highest = p+1
             
     print('[HIGHEST OVERHEADS] DAY: '+data[highest][0]+', AMOUNT: SGD'+str(float(data[highest][1])*x))
